93b413f69b38eef8e65d81e493951af3a02fd40f
Guilhermessimoes01 <sgtgilherme@hotmail.com>
2022-04-02 21:39:31 -0300

    convite

---
+++ convite.js
@@ -0,0 +1,25 @@
+alert("PARABÉNS VOCE FOI CONVIDADO PARA A MELHOR FESTA DE TODAS! CLIQUE EM OK PARA VISUALIZAR O CONVITE")
+
+var n = prompt("para visualizar o convite digite seu nome");
+if(n){
+    alert("seja bem vindo(a) ao seu convite");
+}else{
+    alert("seja bem vindo(a)")
+}
+
+function botao(){
+    alert("obrigado por clicar")
+}
+
+function botao(){
+    window.open("https://www.facebook.com/fabi.simoes.583")
+    //window.location.href="https://www.facebook.com/fabi.simoes.583";
+    
+}
+/*function trocar(){
+    alert("passe o mouse aqui")
+};
+*/
+
+
+//<bottom type="button" onclick="botao()">clique aqui</bottom>
